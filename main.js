5cd9697d1c4af4852d9677545e6fdcd3d68262de
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-05-11 11:15:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,64 @@
 const hiddenLetters = document.getElementById("hidden-letters");
-const themes = document.getElementById("themes");
+const themesContainer = document.getElementById("themes-container");
 const keyboard = document.getElementById("keyboard");
-const newGame = document.getElementById("new-game")
+const restartButton = document.getElementById("new-game");
+const optBtn = document.getElementById("opt-btn");
+// const hangedManFig
+
+let themes_obj = {
+    country: ["Brazil", "Mexico"],
+    sport: ["Football", "Basketball"],
+    object: ["Pen", "Chair"],
+}
+
+function createSecretWord(category) {
+    let random_word = ""
+    if (category == "country") {
+        random_word = themes_obj.country[Math.floor((Math.random() * themes_obj.country.length))];
+        
+    }
+    else if (category == "sport") {
+        random_word = themes_obj.sport[Math.floor((Math.random() * themes_obj.sport.length))];
+    }
+    else if (category == "object") {
+        random_word = themes_obj.object[Math.floor((Math.random() * themes_obj.object.length))];
+    }
+    console.log(random_word);
+}
+
+function showHiddenSpaces() {
+    const 
+}
+
+createSecretWord("object");
+
+// CREATING KEYBOARD
+function generateButtons() {
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('').map(letter =>
+        `
+        <button 
+            class="button" id="` + letter + `"
+            onClick="handleGuess('`+ letter +`')"
+        >
+            ` +letter+ `
+        </button>
+        `
+        ).join('');
+    document.getElementById("keyboard").innerHTML = buttonsHTML;
+}
+
+const btnLocker = () => {
+    let themeButtons = document.querySelectorAll();
+    let letterButtons = document.querySelectorAll();
+}
+
+// STARTER
+const starter = () => {
+    wins_amount = 0;
+    counter = 0;
+}
+
+// RESTART GAME
+generateButtons();
+// restartButton.addEventListener("click", starter);
+// window.onload = starter;
--- teste.js
+++ teste.js
@@ -26,10 +26,12 @@ let options = {
     "Dominica",
   ],
 };
+
 //count
 let winCount = 0;
 let count = 0;
 let chosenWord = "";
+
 //Display option buttons
 const displayOptions = () => {
   optionsContainer.innerHTML += `<h3>Please Select An Option</h3>`;
@@ -39,6 +41,7 @@ const displayOptions = () => {
   }
   optionsContainer.appendChild(buttonCon);
 };
+
 //Block all the Buttons
 const blocker = () => {
   let optionsButtons = document.querySelectorAll(".options");
@@ -134,79 +137,81 @@ const initializer = () => {
   //initialDrawing would draw the frame
   initialDrawing();
 };
-//Canvas
-const canvasCreator = () => {
-  let context = canvas.getContext("2d");
-  context.beginPath();
-  context.strokeStyle = "#000";
-  context.lineWidth = 2;
-  //For drawing lines
-  const drawLine = (fromX, fromY, toX, toY) => {
-    context.moveTo(fromX, fromY);
-    context.lineTo(toX, toY);
-    context.stroke();
-  };
-  const head = () => {
-    context.beginPath();
-    context.arc(70, 30, 10, 0, Math.PI * 2, true);
-    context.stroke();
-  };
-  const body = () => {
-    drawLine(70, 40, 70, 80);
-  };
-  const leftArm = () => {
-    drawLine(70, 50, 50, 70);
-  };
-  const rightArm = () => {
-    drawLine(70, 50, 90, 70);
-  };
-  const leftLeg = () => {
-    drawLine(70, 80, 50, 110);
-  };
-  const rightLeg = () => {
-    drawLine(70, 80, 90, 110);
-  };
-  //initial frame
-  const initialDrawing = () => {
-    //clear canvas
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    //bottom line
-    drawLine(10, 130, 130, 130);
-    //left line
-    drawLine(10, 10, 10, 131);
-    //top line
-    drawLine(10, 10, 70, 10);
-    //small top line
-    drawLine(70, 10, 70, 20);
-  };
-  return { initialDrawing, head, body, leftArm, rightArm, leftLeg, rightLeg };
-};
-//draw the man
-const drawMan = (count) => {
-  let { head, body, leftArm, rightArm, leftLeg, rightLeg } = canvasCreator();
-  switch (count) {
-    case 1:
-      head();
-      break;
-    case 2:
-      body();
-      break;
-    case 3:
-      leftArm();
-      break;
-    case 4:
-      rightArm();
-      break;
-    case 5:
-      leftLeg();
-      break;
-    case 6:
-      rightLeg();
-      break;
-    default:
-      break;
-  }
-};
+
+
+// //Canvas
+// const canvasCreator = () => {
+//   let context = canvas.getContext("2d");
+//   context.beginPath();
+//   context.strokeStyle = "#000";
+//   context.lineWidth = 2;
+//   //For drawing lines
+//   const drawLine = (fromX, fromY, toX, toY) => {
+//     context.moveTo(fromX, fromY);
+//     context.lineTo(toX, toY);
+//     context.stroke();
+//   };
+//   const head = () => {
+//     context.beginPath();
+//     context.arc(70, 30, 10, 0, Math.PI * 2, true);
+//     context.stroke();
+//   };
+//   const body = () => {
+//     drawLine(70, 40, 70, 80);
+//   };
+//   const leftArm = () => {
+//     drawLine(70, 50, 50, 70);
+//   };
+//   const rightArm = () => {
+//     drawLine(70, 50, 90, 70);
+//   };
+//   const leftLeg = () => {
+//     drawLine(70, 80, 50, 110);
+//   };
+//   const rightLeg = () => {
+//     drawLine(70, 80, 90, 110);
+//   };
+//   //initial frame
+//   const initialDrawing = () => {
+//     //clear canvas
+//     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+//     //bottom line
+//     drawLine(10, 130, 130, 130);
+//     //left line
+//     drawLine(10, 10, 10, 131);
+//     //top line
+//     drawLine(10, 10, 70, 10);
+//     //small top line
+//     drawLine(70, 10, 70, 20);
+//   };
+//   return { initialDrawing, head, body, leftArm, rightArm, leftLeg, rightLeg };
+// };
+// //draw the man
+// const drawMan = (count) => {
+//   let { head, body, leftArm, rightArm, leftLeg, rightLeg } = canvasCreator();
+//   switch (count) {
+//     case 1:
+//       head();
+//       break;
+//     case 2:
+//       body();
+//       break;
+//     case 3:
+//       leftArm();
+//       break;
+//     case 4:
+//       rightArm();
+//       break;
+//     case 5:
+//       leftLeg();
+//       break;
+//     case 6:
+//       rightLeg();
+//       break;
+//     default:
+//       break;
+//   }
+// };
 //New Game
 newGameButton.addEventListener("click", initializer);
 window.onload = initializer;
