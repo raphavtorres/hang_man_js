5335fb55018548aad723b98de1043dde5df45f72
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-05-10 22:01:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+const hiddenLetters = document.getElementById("hidden-letters");
+const themes = document.getElementById("themes");
+const keyboard = document.getElementById("keyboard");
+const newGame = document.getElementById("new-game")
