a36334421a4a9445b192c05dd2d9aae2032ee6a6
Raphael Torres <raphaeltorresprofissional@gmail.com>
2023-05-12 08:31:13 -0300

    showing hidden spaces

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const themesContainer = document.getElementById("themes-container");
 const keyboard = document.getElementById("keyboard");
 const restartButton = document.getElementById("new-game");
 const optBtn = document.getElementById("opt-btn");
+
+let secretWord;
+let chosenTheme;
+let dynamicList = [];
+let tryAmount = 6;
 // const hangedManFig
 
 let themes_obj = {
@@ -11,26 +16,45 @@ let themes_obj = {
     object: ["Pen", "Chair"],
 }
 
-function createSecretWord(category) {
-    let random_word = ""
-    if (category == "country") {
+function createSecretWord(theme) {
+    chosenTheme = theme;
+    if (theme == "country") {
         random_word = themes_obj.country[Math.floor((Math.random() * themes_obj.country.length))];
         
     }
-    else if (category == "sport") {
-        random_word = themes_obj.sport[Math.floor((Math.random() * themes_obj.sport.length))];
+    else if (theme == "sport") {
+        secretWord = themes_obj.sport[Math.floor((Math.random() * themes_obj.sport.length))];
     }
-    else if (category == "object") {
-        random_word = themes_obj.object[Math.floor((Math.random() * themes_obj.object.length))];
+    else if (theme == "object") {
+        secretWord = themes_obj.object[Math.floor((Math.random() * themes_obj.object.length))];
     }
-    console.log(random_word);
+    console.log(secretWord)
 }
 
 function showHiddenSpaces() {
-    const 
+    const theme = document.getElementById("theme");
+    theme.innerHTML = chosenTheme;
+    const wordScreen = document.getElementById("hidden-letters");
+
+    for (i = 0; i < secretWord.length; i++) {
+        if (dynamicList[i] == undefined) {
+            dynamicList[i] = "&nbsp;"
+            wordScreen.innerHTML = wordScreen.innerHTML + "<div class='letters'>" + dynamicList[i] + "</div>"
+        } else {
+            wordScreen.innerHTML = wordScreen.innerHTML + "<div class='letters'>" + dynamicList[i] + "</div>"
+        }
+    }
 }
 
-createSecretWord("object");
+function verifyChosenLetter(letter) {
+    if (tryAmount > 0) {
+        changeLetterStyle(letter);
+    }
+}
+
+function changeLetterStyle(letter) {
+    document.getElementById(letter).style.background = "#23243d"
+}
 
 // CREATING KEYBOARD
 function generateButtons() {
@@ -38,7 +62,7 @@ function generateButtons() {
         `
         <button 
             class="button" id="` + letter + `"
-            onClick="handleGuess('`+ letter +`')"
+            onClick="verifyChosenLetter('`+ letter +`')"
         >
             ` +letter+ `
         </button>
@@ -47,16 +71,16 @@ function generateButtons() {
     document.getElementById("keyboard").innerHTML = buttonsHTML;
 }
 
-const btnLocker = () => {
-    let themeButtons = document.querySelectorAll();
-    let letterButtons = document.querySelectorAll();
-}
 
 // STARTER
 const starter = () => {
     wins_amount = 0;
     counter = 0;
 }
+
+// CALLING FUNCTIONS
+createSecretWord("object");
+showHiddenSpaces();
 
 // RESTART GAME
 generateButtons();
